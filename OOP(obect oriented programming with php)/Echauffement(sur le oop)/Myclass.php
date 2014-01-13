0d7009e3e617c36b8c9f0533b8a3382a82075013
aly ba <alybas2002@yahoo.fr>
2014-01-13 14:52:22 +0100

    mises à jour partie PHP aprés suppression des anciens
    delete all the old repo

---
+++ OOP(obect oriented programming with php)/Echauffement(sur le oop)/Myclass.php
@@ -0,0 +1,16 @@
+<?php
+
+class Myclass
+{
+   protected $_myVar = 'my value';
+
+   private function getMyVar()
+   {
+      return $this->myVar;
+   }
+
+   public function get($tableName, $rows)
+   {
+
+   }
+}
---
+++ OOP(obect oriented programming with php)/Echauffement(sur le oop)/index.php
@@ -0,0 +1,22 @@
+<?php include('Myclass.php');
+
+$Myclass = new Myclass();
+$Myclass->myVar = 'new value';
+
+echo $Myclass->get();
+
+
+
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+     <meta charset="utf-8">
+     <title>untitled</title>
+</head>
+<body>
+
+</body>
+</html>
